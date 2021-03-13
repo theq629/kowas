11f7dceda01ad4126c87455877f297d9871cc892
Max Whitney <mwhitney@alumni.sfu.ca>
2021-03-12 18:19:33 -0800

    Use power for base melee attack.

--- src/game/systems/melee.rs
+++ src/game/systems/melee.rs
@@ -1,13 +1,14 @@
 use hecs::Entity;
 use crate::game::state::GameState;
 use crate::game::directions::Direction;
-use crate::game::components::Position;
+use crate::game::components::{Position, Power};
 use super::change::{ChangeResult, ChangeOk};
 use super::damage::melee_damage;
 
 pub fn melee_attack_toward(attacker: Entity, direction: Direction, state: &mut GameState) -> ChangeResult {
+    let attacker_power = state.world.get::<Power>(attacker)?.0;
     let pos = state.world.get::<Position>(attacker)?.0.clone();
     let target_pos = pos + direction.to_point();
-    melee_damage(target_pos, 1, state)?;
+    melee_damage(target_pos, attacker_power, state)?;
     Ok(ChangeOk)
 }
